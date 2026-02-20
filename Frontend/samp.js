2503ee465adebb7dda5632187260792b16c5d2b1
Pratheeba S <pratheebas887@gmail.com>
2026-02-20 16:00:12 +0530

    fe
    Signed-off-by: Pratheeba S <pratheebas887@gmail.com>

---
+++ Frontend/samp.js
@@ -0,0 +1,26 @@
+// function add(e,f) {
+//   let d = e + f;
+//   console.log(e);
+//   console.log(f);
+
+//   console.log(d);
+// }
+
+// add(5,6)
+// add(10,6)
+
+// Map(currentvalue, index , array)
+
+// let values = 5;
+// values = 6;
+
+// let task ="";
+// task = "run";
+// task = "drink";
+
+// setTask("run");
+// task ="run"
+
+// let a=6;
+// console.log(`a value is ${a}`);
+//template literal - backticks - ``
